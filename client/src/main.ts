f52cb33867199e3c41719b5d387b7725caf75788
TFesta1 <festat1@hawkmail.newpaltz.edu>
2023-02-27 14:54:02 -0500

    Import Bulma

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// import 'bulma/css/bulma.css' // Can now modify bulma.css
+
 // Can now modify main.css
 import './assets/main.css'
 
