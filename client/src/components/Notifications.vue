93c78d998fdac18c7c13335c1540cd84080c1328
TFesta1 <festat1@hawkmail.newpaltz.edu>
2023-03-30 18:24:41 -0400

    Fixed Styling Afterclass

--- client/src/components/Notifications.vue
+++ client/src/components/Notifications.vue
@@ -1,51 +1,48 @@
 <script setup lang="ts">
-    import { ref } from "vue";
-    import { useSession, deleteMessage } from "@/model/session";
+    import { ref } from 'vue';
+    import { useSession, deleteMessage } from '@/model/session';
 
     const session = useSession();
-    const isActive = ref(false);
 
+    const isActive = ref(false);
 </script>
 
 <template>
     <div class="notifications">
         <button class="button is-primary" @click="isActive = !isActive">
             <span class="icon">
-                <i class="fas fa-bell fa-lg" :class="{'fa-beat-fade' : session.isLoading}"></i>
+                <i class="fas fa-bell fa-lg " :class="{'fa-beat-fade' : session.isLoading}"></i>
             </span>
         </button>
         <div class="notification-list" v-show="isActive">
             <div v-for="msg, i in session.messages" :class="`notification is-light is-${msg.type}`">
                 <button class="delete" @click="deleteMessage(i)"></button>
                 {{ msg.msg }}
-
             </div>
         </div>
     </div>
 </template>
 
+
 <style scoped>
     .notifications {
         position: relative;
     }
-
-    .notificaiton-list {
+    .notification-list {
         position: absolute;
         margin-top: 5px;
         width: 300px;
         background-color: white;
         border: 1px solid #ccc;
         border-radius: 4px;
-        box-shadow: 0 2px 8px rgba(0, 0, 0, 0.15);
+        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
         z-index: 100;
-        height: 300px;
         min-height: 300px;
         max-height: calc(100vh - 100px);
         overflow-y: auto;
     }
-
-    notification-list .notification {
-        margin: .5px;
+    .notification-list .notification {
+        margin: 5px;
     }
 
 </style>
