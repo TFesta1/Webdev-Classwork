7688ad0689602861858537cbda71f4d412f3af82
TFesta1 <tannerfesta@gmail.com>
2023-04-11 11:49:19 -0400

    Demonstrating Async

---
+++ client/src/components/GenModals.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+    import Modal from "@/components/Modal.vue";
+    import { ref } from "vue";
+
+    const isOpen = ref(false);
+
+    const message = ref("Confirm delete?");
+
+
+
+</script>
+
+<template>
+    <Modal :is-open="isOpen">
+
+        <h1 class="title">{{ message }}</h1>
+        <template #footer>
+            <button class="button is-success">Save changes</button>
+            <button class="button">Cancel</button>
+        </template>
+    </Modal>
+</template>
+
+<style scoped>
+
+</style>
